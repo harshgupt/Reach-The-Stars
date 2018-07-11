02680ed92697a71462685ed43d755d877ab48f97
Harsh Gupta <harshgupt91@gmail.com>
2018-07-11 14:03:33 +0800

    Added character controller script to Bubble

--- Reach The Stars/Assets/Scripts/CharacterController.cs
+++ Reach The Stars/Assets/Scripts/CharacterController.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour {
 
     public GameObject player;
-    public static float speed = 0f;
-    public AudioSource audioSource;
-    public AudioClip appleHit;
-    public AudioClip goldCollect;
-    public AudioClip silverCollect;
-    public AudioClip appleSplat;
 
     void Update ()
     {
@@ -23,19 +17,16 @@ public class CharacterController : MonoBehaviour {
                 touchedPos.y = -4f;
                 if(touchedPos.x >= -2.8f && touchedPos.x <= 2.8f)
                 {
-                    //transform.position = Vector3.Lerp(transform.position, touchedPos, Time.deltaTime * speed);
                     transform.position = touchedPos;
                 }
                 else if(touchedPos.x < -2.8f)
                 {
                     Vector3 leftLimit = new Vector3(-2.8f, -4f, 0);
-                    //transform.position = Vector3.Lerp(transform.position, leftLimit, Time.deltaTime * speed);
                     transform.position = leftLimit;
                 }
                 else if(touchedPos.x > 2.8f)
                 {
                     Vector3 rightLimit = new Vector3(2.8f, -4f, 0);
-                    //transform.position = Vector3.Lerp(transform.position, rightLimit, Time.deltaTime * speed);
                     transform.position = rightLimit;
                 }
             }
@@ -60,11 +51,6 @@ public class CharacterController : MonoBehaviour {
                 transform.position = rightLimit;
             }
         }
-        if (AppleScript.playAudio)
-        {
-            AppleScript.playAudio = false;
-            audioSource.PlayOneShot(appleSplat);
-        }
     }
 
     public void OnMouseDrag()
@@ -75,37 +61,14 @@ public class CharacterController : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Apple")
+        //if(collision.gameObject.tag == "Bouncing Star")
         {
             speed = 0;
-            audioSource.PlayOneShot(appleHit);
             MainScript.isGameOver = true;
             //collision.gameObject.transform.parent = transform;
             collision.GetComponent<SpriteRenderer>().sortingLayerName = "Player";
             collision.tag = "Untagged";
             
         }
-        else if (collision.gameObject.tag == "Silver Apple")
-        {
-            Destroy(collision.gameObject);
-            audioSource.PlayOneShot(silverCollect);
-            OnSilverApple();
-        }
-        else if(collision.gameObject.tag == "Golden Apple")
-        {
-            Destroy(collision.gameObject);
-            audioSource.PlayOneShot(goldCollect);
-            OnGoldenApple();
-        }
-    }
-
-    void OnSilverApple()
-    {
-        ScoreScript.score += 5;
-    }
-
-    void OnGoldenApple()
-    {
-        ScoreScript.score += 10;
     }
 }
