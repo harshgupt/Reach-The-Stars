06fa6660056656c19e944b5e22159ddf2abee1e8
Harsh Gupta <harshgupt91@gmail.com>
2018-07-24 13:21:39 +0800

    Fixed resolution to scale according to screen size

---
+++ Star Bound/Assets/Scripts/ObjectScaler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScaler : MonoBehaviour {
+
+    public float height;
+    public float width;
+    public float ratio;
+
+    public RectTransform rect;
+
+	void Start ()
+    {
+        rect = GetComponent<RectTransform>();
+        height = Screen.height;
+        width = Screen.width;
+        ratio = height / width;
+        transform.localScale = transform.localScale * (ratio) / 1.7778f;
+        rect.anchoredPosition = rect.anchoredPosition * (ratio) / 1.7778f;
+    }
+}
